7f22c5a0765b584752c9c8b37f351b12571ed47c
Emilio Resende <emilio.murta@msitbrasil.com>
2011-06-11 15:09:34 -0300

    adição de novo teste para classe estudante

--- test/br/com/pensino/domain/model/StudentTest.java
+++ test/br/com/pensino/domain/model/StudentTest.java
@@ -4,7 +4,10 @@
  */
 package br.com.pensino.domain.model;
 
+import java.io.File;
 import br.com.caelum.stella.validation.InvalidStateException;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -24,5 +27,21 @@ public class StudentTest {
     @Test(expected = InvalidStateException.class)
     public void studentCantBeCreatedWithInvalidCPF() {
         new Student("Bob", "Marley", "11111111111", "091111");
+    }
+    
+    @Test
+    public void studentCanHaveMultipleFingerprints() throws Exception {
+    Student student = new Student("JOW", "Drungo", "37374688857", "071111");
+    File file = new File("objeto1.ser");
+    FileInputStream fis = new FileInputStream(file.getAbsolutePath());
+    ObjectInputStream ois = new ObjectInputStream(fis);
+    byte[] fingerprintData = (byte[])ois.readObject();
+    byte[] fingerprintData2 = (byte[])ois.readObject();
+    Fingerprint fingeprint = new Fingerprint(fingerprintData, student);
+    student.add(fingerprint
+    Fingerprint fingeprint2 = new Fingerprint(fingerprintData, student);
+    student.add(fingerprint2);
+    assertTrue(student.getFingerprints().contains(fingerprint));
+    assertTrue(student.getFingerprints().contains(fingerprint2));
     }
 }
